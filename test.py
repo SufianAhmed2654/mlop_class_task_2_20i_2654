cae377fa98a4dbe4262efcf6439dbb1a61cfd4cc
SufianAhmed2654 <115447510+SufianAhmed2654@users.noreply.github.com>
2024-02-19 09:58:56 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,17 @@
+import pytest
 from main import StudentsInMLOps
 
-class TestStudentsInMLOps(unittest.TestCase):
-    def setUp(self):
-        self.mlops_class = StudentsInMLOps()
+def test_enrollStudents():
+    mlops = StudentsInMLOps()
+    mlops.enrollStudents(5)
+    assert mlops.getTotalStrength() == 5
 
-    def test_enroll_students(self):
-        self.mlops_class.enrollStudents(10)
-        self.assertEqual(self.mlops_class.getTotalStrength(), 10)
+def test_dropStudents():
+    mlops = StudentsInMLOps()
+    mlops.enrollStudents(10)
+    mlops.dropStudents(3)
+    assert mlops.getTotalStrength() == 7
 
-    def test_drop_students(self):
-        self.mlops_class.enrollStudents(10)
-        self.mlops_class.dropStudents(5)
-        self.assertEqual(self.mlops_class.getTotalStrength(), 5)
-
-    def test_get_total_strength(self):
-        self.mlops_class.enrollStudents(10)
-        self.assertEqual(self.mlops_class.getTotalStrength(), 10)
-
-    def test_get_class_name(self):
-        self.assertEqual(self.mlops_class.getClassName(), "MLOps")
+def test_getClassName():
+    mlops = StudentsInMLOps()
+    assert mlops.getClassName() == "MLOps"
