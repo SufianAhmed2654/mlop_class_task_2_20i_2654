b1454213936c75e3dfab923a1683353f1c46ee1e
SufianAhmed2654 <115447510+SufianAhmed2654@users.noreply.github.com>
2024-02-19 10:00:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 class StudentsInMLOps:
-    def _init_(self):
+    def __init__(self):
         self.total_strength = 0
         self.class_name = "MLOps"
 
-    def enrollStudents(self, num_students):
-        if num_students > 0:
-            self.total_strength += num_students
-            print(f"{num_students} students enrolled in {self.class_name}.")
+    def enrollStudents(self, count):
+        self.total_strength += count
 
-    def dropStudents(self, num_students):
-        if num_students > 0:
-            self.total_strength -= num_students
-            print(f"{num_students} students dropped from {self.class_name}.")
+    def dropStudents(self, count):
+        self.total_strength -= count
 
     def getTotalStrength(self):
         return self.total_strength
